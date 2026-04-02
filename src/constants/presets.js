6b64f6847175216f122e9f4c493167e79d48a076
EeroLai <84887419+EeroLai@users.noreply.github.com>
2026-04-02 10:30:02 +0800

    Add static site source files

---
+++ src/constants/presets.js
@@ -0,0 +1,22 @@
+export const presets = [
+  {
+    key: "coc",
+    baseCooldown: "0.15",
+    labelKey: "hero.presets.coc",
+  },
+  {
+    key: "common",
+    baseCooldown: "0.25",
+    labelKey: "hero.presets.common",
+  },
+  {
+    key: "skill2",
+    baseCooldown: "2",
+    labelKey: "hero.presets.skill2",
+  },
+  {
+    key: "custom",
+    baseCooldown: "",
+    labelKey: "hero.presets.custom",
+  },
+];
